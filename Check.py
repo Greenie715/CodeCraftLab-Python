2ab1055ed62baf5c51555d8fad848bdc515e9578
Greenie715 <cmm111999@gmail.com>
2016-09-14 15:03:46 -0700

    Week 2 python

---
+++ Check.py
@@ -0,0 +1,5 @@
+answer = input ("Do you like Heinz Doofenshmirtz?")
+if answer == "Yes" :
+    print ("Great! Oh, there you are Perry.")
+else:
+    print ("Curse You Perry the Platapus!")
---
+++ bla.py
@@ -0,0 +1,8 @@
+from turtle import *
+shape ("turtle")
+speed(10)
+pensize()
+for count in range(360):
+    forward(1)
+    right(1)
+done()    
